31f2dce302a5fb4095151425174f21d3d40469e2
Arthur Endlein <endarthur@gmail.com>
2020-12-13 22:55:14 -0300

    added autocenter

--- js/autoscale.js
+++ js/autoscale.js
@@ -13,3 +13,19 @@ AFRAME.registerComponent("autoscale", {
     });
   },
 });
+
+AFRAME.registerComponent("autocenter", {
+  schema: {type:"vec3", default: {x: 0, y: 0, z: 0} },
+
+  init: function () {
+    var el = this.el;
+    var self = this;
+    var offset = new THREE.Vector3();
+    var box = new THREE.Box3();
+    this.el.addEventListener("model-loaded", function(){
+      box.setFromObject(el.object3D);
+      box.getCenter(offset).setY(box.min.y).negate().add(self.data);
+      el.object3D.position.add(offset);
+    });
+  },
+});
